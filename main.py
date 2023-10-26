4c1356b2df3ab9c7118a5623931573fc30897e21
bongkyunSON <114561525+bongkyunSON@users.noreply.github.com>
2023-10-26 12:25:13 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from dotenv import load_dotenv
+load_dotenv()
+from langchain.chat_models import ChatOpenAI
+
+chat_model = ChatOpenAI()
+
+st.title('인공지능 시안')
+
+
+
+
+content = st.chat_input("입력해주세요")
+# if content:
+#     st.write(f"User has sent the following prompt: {prompt}")
+
+
+if content:
+    with st.spinner('잠시만 기다려 주세요...'):
+        result = chat_model.predict(content + "에 대한 시를 써줘")
+        st.write(result)
+
+
+
+
+
